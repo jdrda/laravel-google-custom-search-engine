d5e508954870c9d04cb4c16813cabb3e36fc56c9
Paul Przyborski <przybang@users.noreply.github.com>
2019-10-07 16:45:07 -0400

    Update LaravelGoogleCustomSearchEngine.php
    This edit updates the URL and allows for the Google Restricted JSON API usage to you don't run into query limits.

--- src/JanDrda/LaravelGoogleCustomSearchEngine/LaravelGoogleCustomSearchEngine.php
+++ src/JanDrda/LaravelGoogleCustomSearchEngine/LaravelGoogleCustomSearchEngine.php
@@ -118,7 +118,7 @@ class LaravelGoogleCustomSearchEngine
          */
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/customsearch/v1" . $searchArray);
+        curl_setopt($ch, CURLOPT_URL, "https://www.googleapis.com/customsearch/v1/siterestrict" . $searchArray);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
